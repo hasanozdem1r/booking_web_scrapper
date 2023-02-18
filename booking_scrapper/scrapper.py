01d7856f0f79df53555807eb80f5a6d1c6f3d19b
Hasan Özdemir <hasanozdemir1@trakya.edu.tr>
2023-02-18 19:06:47 +0300

    fix: constructor fixed, in case of HTTP Error program will raise error

--- booking_scrapper/scrapper.py
+++ booking_scrapper/scrapper.py
@@ -1,21 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 from typing import Dict
+from requests.exceptions import HTTPError
 
-BASE_LINK = "http://127.0.0.1:8080/data/Kempinski%20Hotel%20Bristol%20Berlin%2C%20Germany%20-%20Booking.com.html"
+BASE_LINK = "http://localhost:8000/data/Kempinski%20Hotel%20Bristol%20Berlin%2C%20Germany%20-%20Booking.com.html"
 
 
 class BookingScrapper:
 
     def __init__(self, base_link: str) -> None:
-        # use fake User-Agent to deal 403 Forbidden
-        headers: Dict[str, str] = {
-            'User-Agent':
-                'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
-        }
-        self.page_content = requests.get(base_link, headers=headers).content
-        print(self.page_content)
+        try:
+            # use fake User-Agent to deal 403 Forbidden
+            headers: Dict[str, str] = {
+                'User-Agent':
+                    'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+            }
+            response = requests.get(base_link, headers=headers, timeout=30)
+            # If the response was successful, no Exception will be raised
+            response.raise_for_status()
+            self.content = response.content
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
 
+    def __scrape_hotel_details(self):
+        pass
 
 if __name__ == "__main__":
     booking_scrapper = BookingScrapper(base_link=BASE_LINK)
