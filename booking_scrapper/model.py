afc532727f030f3ee02a3edebe0cb22678a96067
Hasan Özdemir <hasanozdemir1@trakya.edu.tr>
2023-02-21 02:17:20 +0300

    fix: validators added, code refactored

--- booking_scrapper/model.py
+++ booking_scrapper/model.py
@@ -2,11 +2,10 @@
 Pydantic Classes for metadata of hotels, it allows you validate and easily serialize
 @hasanozdem1r 02-18-2023
 """
-from pydantic import BaseModel, validator, schema_json_of
+from pydantic import BaseModel, validator, schema_json_of, AnyHttpUrl
 from typing import List
 
 
-# TODO: validator tbd
 # base class for HotelMinified and HotelExtended
 class Hotel(BaseModel):
     hotel_name: str
@@ -14,17 +13,39 @@ class Hotel(BaseModel):
     number_of_reviewers: int
     review_points: float
 
+    @validator('number_of_reviewers')
+    def number_of_reviewers_must_be_positive(cls,value):
+        if value<0:
+            raise ValueError("Reviewers cannot be less than 0")
+        return value
+    
+    @validator('review_points')
+    def review_points_must_be_between_zero_ten(cls,value):
+        if not 0.0<=value<=10.0:
+            raise ValueError("Hotel ratings must be between 0.0 and 10.0")
+        return value
+
 
 class HotelRoom(BaseModel):
     room_capacity: int  # named Max on booking.com
     room_type: str  # short description of room
-    price_link: str  # if person want to access prices of room
+    price_link: AnyHttpUrl  # if person want to access prices of room
 
+    @validator('room_capacity')
+    def room_capacity_must_be_positive(cls,value):
+        if value<0:
+            raise ValueError("Room capacity cannot be less than 0")
+        return value
 
 class HotelMinified(Hotel):
-    booking_link: str  # if person want to go link of hotel
+    booking_link: AnyHttpUrl  # scheme http or https, TLD not required, host required
     number_of_visitors: int  # how many people looking to hotel
 
+    @validator('number_of_visitors')
+    def number_of_visitors_must_be_positive(cls,value):
+        if value<0:
+            raise ValueError("Visitors cannot be less than 0")
+        return value
 
 class HotelExtended(Hotel):
     address: str
