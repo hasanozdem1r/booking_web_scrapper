0b1d6495c878452f90c4a15011efd770b66b7bc6
Hasan Özdemir <hasanozdemir1@trakya.edu.tr>
2023-02-21 21:55:43 +0300

    fix: models switched to class and by valid and invalid data methods re-written

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,55 +1,103 @@
-from typing import Dict
 import pytest
+from booking_scraper.exception import ModelValidationError
+from booking_scraper.model import Hotel, HotelRoom, HotelMinified, HotelExtended
 
-from booking_scraper.model import Hotel, HotelMinified, HotelExtended, HotelRoom
 
+class TestHotel:
+    def test_number_of_reviews_must_be_positive(self):
+        with pytest.raises(ModelValidationError):
+            Hotel(
+                hotel_name="Example Hotel",
+                description="A nice hotel",
+                number_of_reviews=-5,
+                review_points=8.5,
+            )
 
-def test_invalid_number_of_reviews_validator():
-    with pytest.raises(ValueError):
-        Hotel(
-            hotel_name="Test Hotel",
-            description="A hotel for testing",
-            number_of_reviews=-1,
-            review_points=8.0,
+    def test_review_points_must_be_between_zero_ten(self):
+        with pytest.raises(ModelValidationError):
+            Hotel(
+                hotel_name="Example Hotel",
+                description="A nice hotel",
+                number_of_reviews=20,
+                review_points=12.5,
+            )
+
+    def test_can_create_hotel_with_valid_data(self):
+        hotel = Hotel(
+            hotel_name="Example Hotel",
+            description="A nice hotel",
+            number_of_reviews=20,
+            review_points=8.5,
         )
+        assert hotel.hotel_name == "Example Hotel"
+        assert hotel.description == "A nice hotel"
+        assert hotel.number_of_reviews == 20
+        assert hotel.review_points == 8.5
 
 
-def test_invalid_review_points_validator():
-    with pytest.raises(ValueError):
-        Hotel(
-            hotel_name="Test Hotel",
-            description="A hotel for testing",
-            number_of_reviews=10,
-            review_points=11.0,
+class TestHotelRoom:
+    def test_room_capacity_must_be_positive(self):
+        with pytest.raises(ModelValidationError):
+            HotelRoom(room_capacity={"adult": 2, "children": -1}, room_type="Double")
+
+    def test_can_create_hotel_room_with_valid_data(self):
+        room = HotelRoom(room_capacity={"adult": 2, "children": 1}, room_type="Double")
+        assert room.room_capacity == {"adult": 2, "children": 1}
+        assert room.room_type == "Double"
+
+
+class TestHotelMinified:
+    def test_number_of_visitors_must_be_positive(self):
+        with pytest.raises(ModelValidationError):
+            HotelMinified(
+                hotel_name="Example Hotel",
+                description="A nice hotel",
+                number_of_reviews=20,
+                review_points=8.5,
+                booking_link="http://example.com",
+                number_of_visitors=-1,
+            )
+
+    def test_can_create_hotel_minified_with_valid_data(self):
+        hotel = HotelMinified(
+            hotel_name="Example Hotel",
+            description="A nice hotel",
+            number_of_reviews=20,
+            review_points=8.5,
+            booking_link="http://example.com",
+            number_of_visitors=5,
         )
+        assert hotel.hotel_name == "Example Hotel"
+        assert hotel.description == "A nice hotel"
+        assert hotel.number_of_reviews == 20
+        assert hotel.review_points == 8.5
+        assert hotel.booking_link == "http://example.com"
+        assert hotel.number_of_visitors == 5
 
 
-def test_invalid_room_capacity_validator():
-    with pytest.raises(ValueError):
-        HotelRoom(room_type="single", room_capacity={"adult": -1, "children": 1})
-
-
-def test_invalid_number_of_visitors_validator():
-    with pytest.raises(ValueError):
-        HotelMinified(
-            hotel_name="Test Hotel",
-            description="A hotel for testing",
-            number_of_reviews=10,
-            review_points=8.0,
-            booking_link="https://www.test.com",
-            number_of_visitors=-1,
+class TestHotelExtended:
+    def test_can_create_hotel_extended_with_valid_data(self):
+        room1 = HotelRoom(room_capacity={"adult": 2, "children": 1}, room_type="Double")
+        room2 = HotelRoom(room_capacity={"adult": 1, "children": 0}, room_type="Single")
+        hotel_minified = HotelMinified(
+            hotel_name="Example Hotel",
+            description="A nice hotel",
+            number_of_reviews=20,
+            review_points=8.5,
+            booking_link="http://example.com",
+            number_of_visitors=5,
         )
-
-
-def test_invalid_all_hotel_extended():
-    with pytest.raises(ValueError):
-        HotelExtended(
-            hotel_name="Test Hotel",
-            description="A hotel for testing",
-            number_of_reviews=-10,
-            review_points=-8.0,
+        hotel_extended = HotelExtended(
+            hotel_name="Example Hotel",
+            description="A nice hotel",
+            number_of_reviews=20,
+            review_points=8.5,
             address="123 Main St.",
             classification="4 stars",
-            room_categories=[],
-            alternative_hotels=[],
+            room_categories=[room1, room2],
+            alternative_hotels=[hotel_minified],
         )
+        assert hotel_extended.hotel_name == "Example Hotel"
+        assert hotel_extended.description == "A nice hotel"
+        assert hotel_extended.number_of_reviews == 20
+        assert hotel_extended.review_points == 8.5
