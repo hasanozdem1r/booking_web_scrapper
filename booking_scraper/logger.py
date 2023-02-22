e9289d3fd89cbf4074d0fa541e6bc381fe7967a0
Hasan Özdemir <hasanozdemir1@trakya.edu.tr>
2023-02-22 15:59:45 +0300

    feature: booking_scraper logger class is implemented for info, warning, error

---
+++ booking_scraper/logger.py
@@ -0,0 +1,58 @@
+import logging
+from rich.logging import RichHandler
+
+logging.basicConfig(
+    level="NOTSET",
+    format="%(asctime)s %(levelname)s: %(message)s",
+    datefmt="%d/%b/%Y:%H:%M:%S %z", # format of a standard HTTP
+    handlers=[RichHandler(rich_tracebacks=True)], # RichHandler format and colorize text written by Python’s logging
+)
+
+class ScraperLogging(object):
+    def __init__(
+        self,
+        name: str = "SCRAPER",
+        file_name: str = "booking_scraper.log",
+        file_mode: str = "a",
+        level: str = "NOTSET",
+        format: str = "%(asctime)s %(levelname)s: %(message)s",
+        date_format: str = "%d/%b/%Y:%H:%M:%S %z",
+        traceback: bool = True,
+    ) -> None:
+        self.name = name
+        self.file_name = file_name
+        self.file_mode = file_mode
+        self.level = level
+        self.format = format
+        self.date_format = date_format
+        self.traceback = traceback
+
+    def __setup_loger(self):
+        logging.basicConfig(
+            filename=self.file_name,
+            filemode=self.file_mode,
+            level=self.level,
+            format=self.format,
+            datefmt=self.date_format,
+            handlers=[RichHandler(rich_tracebacks=self.traceback)],
+        )
+        self.logger = logging.getLogger(self.name)
+
+    def info(self, message: str, traceback: bool = False):
+        if traceback is not None:
+            self.traceback = traceback
+            self.__setup_loger()
+        return self.logger.info(message)
+
+    def warning(self, message: str, traceback: bool = False):
+        if traceback is not None:
+            self.traceback = traceback
+            self.__setup_loger()
+        return self.logger.warning(message)
+
+    def critical(self, message: str, traceback: bool = False):
+        if traceback is not None:
+            self.traceback = traceback
+            self.__setup_loger()
+        return self.logger.critical(message)
+
